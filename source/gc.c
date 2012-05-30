8c9ffd99a4d688e1e2c6c96302c146d1cebb79ac
fix94.1@gmail.com <fix94.1@gmail.com@7a4f0277-606d-807d-2b8a-13b88abf7aac>
2012-05-30 13:25:09 +0000

    v0.2 sources, options can be set in defines.h now

--- source/gc.c
+++ source/gc.c
@@ -1,55 +1,75 @@
 #include <gccore.h>
+#include <string.h>
+#include <malloc.h>
+#include <stdio.h>
 #include "gc.h"
-
-#define SRAM_ENGLISH 0
-#define SRAM_GERMAN 1
-#define SRAM_FRENCH 2
-#define SRAM_SPANISH 3
-#define SRAM_ITALIAN 4
-#define SRAM_DUTCH 5
+#include "defines.h"
 
 syssram* __SYS_LockSram();
 u32 __SYS_UnlockSram(u32 write);
 u32 __SYS_SyncSram(void);
 
-void set_video_mode(int i)
+void GC_SetVideoMode(u8 videomode)
 {
 	syssram *sram;
 	sram = __SYS_LockSram();
 	void *m_frameBuf;
 	static GXRModeObj *rmode;
+	int memflag = 0;
 
-	if(VIDEO_HaveComponentCable())
+	if((VIDEO_HaveComponentCable() && (CONF_GetProgressiveScan() > 0)) || videomode > 3)
 		sram->flags |= 0x80; //set progressive flag
 	else
 		sram->flags &= 0x7F; //clear progressive flag
 
-	if (!i)
+	if(videomode == 1 || videomode == 3 || videomode == 5)
 	{
-		rmode = &TVNtsc480IntDf;
-		sram->flags &= 0xFE; // Clear bit 0 to set the video mode to NTSC
-		sram->ntd &= 0xBF; //clear pal60 flag
+		memflag = 1;
+		sram->flags |= 0x01; // Set bit 0 to set the video mode to PAL
+		sram->ntd |= 0x40; //set pal60 flag
 	}
 	else
 	{
+		sram->flags &= 0xFE; // Clear bit 0 to set the video mode to NTSC
+		sram->ntd &= 0xBF; //clear pal60 flag
+	}
+
+	if(videomode == 1)
 		rmode = &TVPal528IntDf;
-		sram->flags |= 0x01; // Set bit 0 to set the video mode to PAL
-		sram->ntd |= 0x40; //set pal60 flag
+	else if(videomode == 2)
+		rmode = &TVNtsc480IntDf;
+	else if(videomode == 3)
+	{
+		rmode = &TVEurgb60Hz480IntDf;
+		memflag = 5;
+	}
+	else if(videomode == 4)
+		rmode = &TVNtsc480Prog;
+	else if(videomode == 5)
+	{
+		rmode = &TVNtsc480Prog; //TVEurgb60Hz480Prog codedumps
+		memflag = 5;
 	}
 
 	__SYS_UnlockSram(1); // 1 -> write changes
 	while(!__SYS_SyncSram());
 
 	/* Set video mode to PAL or NTSC */
-	*(vu32*)0x800000CC = i;
-	DCFlushRange((void *)(0x800000CC), 1);
-	ICInvalidateRange((void *)(0x800000CC), 1);
-	
+	*(vu32*)0x800000CC = memflag;
+	DCFlushRange((void *)(0x800000CC), 4);
+	ICInvalidateRange((void *)(0x800000CC), 4);
+
 	VIDEO_Configure(rmode);
 	m_frameBuf = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
-	
+
 	VIDEO_ClearFrameBuffer(rmode, m_frameBuf, COLOR_BLACK);
 	VIDEO_SetNextFramebuffer(m_frameBuf);
+
+	VIDEO_SetBlack(TRUE);
+	VIDEO_Flush();
+	VIDEO_WaitVSync();
+	if(rmode->viTVMode&VI_NON_INTERLACE) 
+		VIDEO_WaitVSync();
 }
 
 u8 get_wii_language()
@@ -71,12 +91,10 @@ u8 get_wii_language()
 	}
 }
 
-void set_language(u8 lang)
+void GC_SetLanguage(u8 lang)
 {
 	if (lang == 0)
-	{
 		lang = get_wii_language();
-	}
 	else
 		lang--;
 
@@ -86,4 +104,72 @@ void set_language(u8 lang)
 
 	__SYS_UnlockSram(1); // 1 -> write changes
 	while(!__SYS_SyncSram());
+}
+
+
+void DML_New_SetOptions(char *GamePath)
+{
+	DML_CFG *DMLCfg = (DML_CFG*)memalign(32, sizeof(DML_CFG));
+	memset(DMLCfg, 0, sizeof(DML_CFG));
+
+	DMLCfg->Magicbytes = 0xD1050CF6;
+	DMLCfg->CfgVersion = 0x00000001;
+	//DMLCfg->VideoMode |= DML_VID_FORCE;
+	DMLCfg->VideoMode |= DML_VID_NONE;
+
+	if(GamePath != NULL)
+	{
+		snprintf(DMLCfg->GamePath, sizeof(DMLCfg->GamePath), "/games/%s/game.iso", GamePath);
+		DMLCfg->Config |= DML_CFG_GAME_PATH;
+	}
+
+	#ifdef ACTIVITYLED
+		DMLCfg->Config |= DML_CFG_ACTIVITY_LED;
+	#endif
+
+	#ifdef PADRESET
+		DMLCfg->Config |= DML_CFG_PADHOOK;
+	#endif
+
+	#ifdef CHEATS
+		DMLCfg->Config |= DML_CFG_CHEATS;
+	#endif
+
+	#ifdef NMM
+		DMLCfg->Config |= DML_CFG_NMM;
+	#endif
+
+	#ifdef NODISC
+		DMLCfg->Config |= DML_CFG_NODISC;
+	#endif
+
+	/*
+	if(DMLvideoMode == 1)
+		DMLCfg->VideoMode |= DML_VID_FORCE_PAL50;
+	else if(DMLvideoMode == 2)
+		DMLCfg->VideoMode |= DML_VID_FORCE_NTSC;
+	else if(DMLvideoMode == 3)
+		DMLCfg->VideoMode |= DML_VID_FORCE_PAL60;
+	else
+		DMLCfg->VideoMode |= DML_VID_FORCE_PROG;
+	*/
+
+	//Write options into memory
+	memcpy((void *)0xC0001700, DMLCfg, sizeof(DML_CFG));
+	free(DMLCfg);
+}
+
+void DML_Old_SetOptions(char *GamePath)
+{
+	FILE *f;
+	f = fopen("sd:/games/boot.bin", "wb");
+	fwrite(GamePath, 1, strlen(GamePath) + 1, f);
+	fclose(f);
+
+	//Tell DML to boot the game from sd card
+	*(vu32*)0x80001800 = 0xB002D105;
+	DCFlushRange((void *)(0x80001800), 4);
+	ICInvalidateRange((void *)(0x80001800), 4);
+
+	*(vu32*)0xCC003024 |= 7;
 }
