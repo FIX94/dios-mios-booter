d13fd8524a757cc6f37b75ea73d97d3e439dcfc5
fix94.1@gmail.com <fix94.1@gmail.com@7a4f0277-606d-807d-2b8a-13b88abf7aac>
2012-05-30 13:29:36 +0000

    v0.5 sources, new better menu and autoboot possibility

--- trunk/source/defines.h
+++ trunk/source/defines.h
@@ -4,11 +4,17 @@
 //If you have a DML version under r52
 //#define OLD_DML
 
-//Change that to the game folder name in sd:/games
-//#define Game "GZLP01"
+//Comment that out for automatically game booting
+//#define AUTOBOOT
+
+//Change that to the game folder name in sd:/games (only works if AUTOBOOT is enabled)
+#define Game "GZLP01"
 
 //Comment that out for NTSC Video Mode
 #define PAL_VIDEO_MODE
+
+//Comment that out to always use the selected video mode, leave it like this and the booter choose the mode
+//define FORCE_VIDEO_MODE
 
 //Only works with new DML
 #define CHEATS
