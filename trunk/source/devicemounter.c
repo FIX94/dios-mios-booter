eaaf3ed39be72b26686c58090afcb76960cfd42a
fix94.1@gmail.com <fix94.1@gmail.com@7a4f0277-606d-807d-2b8a-13b88abf7aac>
2012-06-24 21:05:59 +0000

    -added support for using config on usb device if no sd card is found, now also renamed the ini to dios-mios-booter.ini, it will be on the device root instead of the games folder now

--- trunk/source/devicemounter.c
+++ trunk/source/devicemounter.c
@@ -19,6 +19,9 @@
 #define CACHE 8
 #define SECTORS 64
 
+bool _SD_Inited = false;
+bool _USB_Inited = false;
+
 typedef struct _PARTITION_RECORD {
 	u8 status;                              /* Partition status; see above */
 	u8 chs_start[3];                        /* Cylinder-head-sector address to first block of partition */
@@ -39,7 +42,7 @@ typedef struct _MASTER_BOOT_RECORD {
 #define le32(i) (((((u32) i) & 0xFF) << 24) | ((((u32) i) & 0xFF00) << 8) | \
 				((((u32) i) & 0xFF0000) >> 8) | ((((u32) i) & 0xFF000000) >> 24))
 
-int USBDevice_Init()
+s32 USBDevice_Init()
 {
 	time_t start = time(0);
 
@@ -47,7 +50,6 @@ int USBDevice_Init()
 	{
 		if(__io_usbstorage.startup() && __io_usbstorage.isInserted())
 			break;
-
 		usleep(200000); // 1/5 sec
 	}
 
@@ -69,13 +71,16 @@ int USBDevice_Init()
 
 		__io_usbstorage.readSectors(le32(mbr->partitions[i].lba_start), 1, BootSector);
 
-		if(*((u16 *) (BootSector + 0x1FE)) == 0x55AA)
+		if(*((u16 *)(BootSector + 0x1FE)) == 0x55AA)
 		{
 			//! Partition typ can be missleading the correct partition format. Stupid lazy ass Partition Editors.
 			if(memcmp(BootSector + 0x36, "FAT", 3) == 0 || memcmp(BootSector + 0x52, "FAT", 3) == 0)
 			{
+				free(BootSector);
 				fatMount(DeviceName[USB], &__io_usbstorage, le32(mbr->partitions[i].lba_start), CACHE, SECTORS);
-				break;
+				free(mbr);
+				_USB_Inited = true;
+				return 1;
 			}
 		}
     }
@@ -85,23 +90,37 @@ int USBDevice_Init()
 	return -1;
 }
 
+bool USBDevice_Inited()
+{
+	return _USB_Inited;
+}
+
 void USBDevice_deInit()
 {
 	fatUnmount("usb:/");
 	//Let's not shutdown so it stays awake for the application
 	__io_usbstorage.shutdown();
 	USB_Deinitialize();
+	_USB_Inited = false;
 }
 
-int SDCard_Init()
+s32 SDCard_Init()
 {
 	if(!__io_wiisd.startup() || !__io_wiisd.isInserted())
 		return -1;
 
 	if(fatMount(DeviceName[SD], &__io_wiisd, 0, CACHE, SECTORS))
+	{
+		_SD_Inited = true;
 		return 1;
+	}
 
 	return -1;
+}
+
+bool SDCard_Inited()
+{
+	return _SD_Inited;
 }
 
 void SDCard_deInit()
@@ -110,4 +129,5 @@ void SDCard_deInit()
 	fatUnmount("sd:/");
 	//Let's not shutdown so it stays awake for the application
 	__io_wiisd.shutdown();
+	_SD_Inited = false;
 }
